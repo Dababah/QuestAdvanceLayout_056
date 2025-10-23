6ed6d75dd84be58af6612b7583303001c9765945
Dababah <pawasalee96@gamil.com>
2025-10-23 13:35:12 +0700

    nambah box buat copyright

--- app/src/main/java/com/example/layout3/UIPertama.kt
+++ app/src/main/java/com/example/layout3/UIPertama.kt
@@ -77,8 +77,7 @@ fun ActivitasPertama (modifier: Modifier){
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            Text(
-                stringResource(R.string.copy),
+            Text(stringResource(R.string.copy),
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .padding(bottom = 50.dp)
