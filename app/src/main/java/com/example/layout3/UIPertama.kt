9679c0931fa462dac3493a53bd55ba0951a640f7
Dababah <pawasalee96@gamil.com>
2025-10-23 13:33:49 +0700

    nambah teks alamat

--- app/src/main/java/com/example/layout3/UIPertama.kt
+++ app/src/main/java/com/example/layout3/UIPertama.kt
@@ -65,7 +65,7 @@ fun ActivitasPertama (modifier: Modifier){
                         modifier = Modifier.padding(top=15.dp)
                     )
                     Text(
-                        stringResource(R.string.alamat),
+                        Text(stringResource(R.string.alamat)),
                         fontSize = 20.sp,
                         color = Color.Yellow,
                         modifier = Modifier.padding(top=10.dp)
